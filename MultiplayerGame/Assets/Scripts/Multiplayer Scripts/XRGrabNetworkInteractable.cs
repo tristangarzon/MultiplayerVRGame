bac8cffb042fde67b3c0f1c41a06882b3a48b478
Tristan Garzon <47580382+tristangarzon@users.noreply.github.com>
2021-04-06 15:51:04 -0400

    Interactable Objects & Voice Chat
    Objects:
    - Made an object which is interactable with the player
    - Objects velocity & angular velocity is sync'd over the server making multiple people able to interact with the same objects
    
    Voice:
    - Added voice chat using Photon Voice 2
    - Allows the player to speak in game and be heard by other players
    - Audio is set to spatial and has a max distance, supposed to simulate real life

---
+++ MultiplayerGame/Assets/Scripts/Multiplayer Scripts/XRGrabNetworkInteractable.cs
@@ -0,0 +1,48 @@
+﻿/*
+* Made by: Tristan Garzon
+* 
+* Script Summary:
+*
+*
+* When an object has been grabbed the ownership of the object switches from netural to being owned by the player
+* who grabs said object. When a new player decides to grab that object, the ownership of the objects now 
+* needs to be switched to the new player attempting to grab the object.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using Photon.Pun;
+
+public class XRGrabNetworkInteractable : XRGrabInteractable
+{
+    #region Variables
+    private PhotonView photonView; 
+    #endregion
+	
+    #region Unity Methods
+
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();    //Stores a reference to the photonview component
+    }
+
+   
+    void Update()
+    {
+        
+    }
+
+    protected override void OnSelectEntered(XRBaseInteractor interactor) //Called when an object is being grabbed
+    {
+        //Request ownership of an object
+       
+        photonView.RequestOwnership();
+
+        base.OnSelectEntered(interactor);
+    }
+
+
+    #endregion
+}
