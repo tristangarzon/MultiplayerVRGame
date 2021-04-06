d0d92a3c1df6dc3606dd247718a2fd9305aad6fb
Tristan Garzon <47580382+tristangarzon@users.noreply.github.com>
2021-04-06 03:23:34 -0400

    Bug Fixes & Player Hand  Animations
    Bug:
    - Network player script was tracking the local position of the player, updated so now it tracks the world position of the player
    
    Player:
    - Updated the animation of the Players hands, now when the player squeezes his hands the animation will match.
    - Now Syncs the hand animation over the network

--- MultiplayerGame/Assets/Scripts/Multiplayer Scripts/NetworkPlayer.cs
+++ MultiplayerGame/Assets/Scripts/Multiplayer Scripts/NetworkPlayer.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using Photon.Pun;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class NetworkPlayer : MonoBehaviour
 {
@@ -17,6 +18,14 @@ public class NetworkPlayer : MonoBehaviour
     public Transform head;          //Reference to the player Head
     public Transform leftHand;      //Reference to the player Left Hand
     public Transform rightHand;     //Reference to the player Right Hand
+
+    public Animator leftHandAnimator;
+    public Animator rightHandAnimator;
+
+
+    private Transform headRig;
+    private Transform leftHandRig;
+    private Transform rightHandRig;
 
     private PhotonView photonView;
     #endregion
@@ -26,6 +35,25 @@ public class NetworkPlayer : MonoBehaviour
     void Start()
     {
         photonView = GetComponent<PhotonView>();
+
+        //Locates the XRRig in the scene
+        XRRig rig = FindObjectOfType<XRRig>();
+        //Stores the transform of the Camera 
+        headRig = rig.transform.Find("Camera Offset/Main Camera");
+        //Store the transform of the Left Hand 
+        leftHandRig = rig.transform.Find("Camera Offset/LeftHand Controller");
+        //Store the transform of the Left Hand 
+        rightHandRig = rig.transform.Find("Camera Offset/RightHand Controller");
+
+        if(photonView.IsMine)
+        {
+            foreach (var item in GetComponentsInChildren<Renderer>()) //Returns all the renderer components of the network player
+            {
+                //Disables the renderer
+                item.enabled = false;
+            }
+        }
+
     }
 
    
@@ -35,33 +63,54 @@ public class NetworkPlayer : MonoBehaviour
         //If it's the users prefab then Update the following
         if(photonView.IsMine)
         {
-           
-            rightHand.gameObject.SetActive(false);
-            leftHand.gameObject.SetActive(false);
-            head.gameObject.SetActive(false);
 
             //Updates the Position/Rotation of the Head
-            MapPosition(head, XRNode.Head);
+            MapPosition(head, headRig);
             //Updates the Position/Rotation of the Left Hand
-            MapPosition(leftHand, XRNode.LeftHand);
+            MapPosition(leftHand, leftHandRig);
             //Updates the Position/Rotation of the Right Hand
-            MapPosition(rightHand, XRNode.RightHand);
+            MapPosition(rightHand, rightHandRig);
+
+
+            //Updates the animation of the Left Hand
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.LeftHand), leftHandAnimator);
+            //Updates the animation of the Right Hand
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.RightHand), rightHandAnimator);
+
         }
 
-      
+
     }
 
-    void MapPosition(Transform target, XRNode node) //Syncs the position of the XR device with the ingame player
+    void UpdateHandAnimation(InputDevice targetDevice, Animator handAnimator)  //Updates the hand animation
     {
-        //Gets the position of the XR device
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
-        //Gets the rotation of the XR device
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+    }
+
+    void MapPosition(Transform target, Transform rigTransform) //Syncs the position of the XR device with the ingame player
+    {
+       
 
         //Sets the position to the target
-        target.position = position;
+        target.position = rigTransform.position;
         //Sets the rotation to the target
-        target.rotation = rotation;
+        target.rotation = rigTransform.rotation;
     }
 
     #endregion
