85e994eae5acc1dac50a2678cda81ddf2499a4a0
Tristan Garzon <47580382+tristangarzon@users.noreply.github.com>
2021-04-06 02:53:27 -0400

    Multiplayer Rooms & Demo network model
    Multiplayer:
    - Connects the player to the room
    - Notifies players when someone leaves the room
    
    Player:
    - Made a model for the character when they connect to the server
    - Sync'd the players movement over the network

---
+++ MultiplayerGame/Assets/Scripts/Multiplayer Scripts/NetworkPlayer.cs
@@ -0,0 +1,68 @@
+﻿/*
+* Made by: Tristan Garzon
+* 
+* Script Summary:
+*
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using Photon.Pun;
+
+public class NetworkPlayer : MonoBehaviour
+{
+    #region Variables
+    public Transform head;          //Reference to the player Head
+    public Transform leftHand;      //Reference to the player Left Hand
+    public Transform rightHand;     //Reference to the player Right Hand
+
+    private PhotonView photonView;
+    #endregion
+
+    #region Unity Methods
+
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+   
+    void Update()
+    {
+        //Checks to see which prefab component is being used 
+        //If it's the users prefab then Update the following
+        if(photonView.IsMine)
+        {
+           
+            rightHand.gameObject.SetActive(false);
+            leftHand.gameObject.SetActive(false);
+            head.gameObject.SetActive(false);
+
+            //Updates the Position/Rotation of the Head
+            MapPosition(head, XRNode.Head);
+            //Updates the Position/Rotation of the Left Hand
+            MapPosition(leftHand, XRNode.LeftHand);
+            //Updates the Position/Rotation of the Right Hand
+            MapPosition(rightHand, XRNode.RightHand);
+        }
+
+      
+    }
+
+    void MapPosition(Transform target, XRNode node) //Syncs the position of the XR device with the ingame player
+    {
+        //Gets the position of the XR device
+        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
+        //Gets the rotation of the XR device
+        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
+
+        //Sets the position to the target
+        target.position = position;
+        //Sets the rotation to the target
+        target.rotation = rotation;
+    }
+
+    #endregion
+}
---
+++ MultiplayerGame/Assets/Scripts/Multiplayer Scripts/NetworkPlayerSpawner.cs
@@ -0,0 +1,39 @@
+﻿/*
+* Made by: Tristan Garzon
+* 
+* Script Summary:
+* 
+* Handles the spawning of other player
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
+{
+    #region Variables
+    private GameObject spawnedPlayerPrefab;
+    #endregion
+
+    #region Unity Methods
+
+    public override void OnJoinedRoom() //Called when a player joins a room
+    {
+        base.OnJoinedRoom();
+
+        //Spawns a player into the world. Player must have the same name as the one linked below
+        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", transform.position, transform.rotation);
+    }
+
+    public override void OnLeftRoom() //Called when a player leaves the room
+    {
+        base.OnLeftRoom();
+
+        //Destorys the spawnedPlayerPrefab after they have left the room
+        PhotonNetwork.Destroy(spawnedPlayerPrefab);
+    }
+
+    #endregion
+}
