f59a408d216a9c281f8081d306acaeb5a0e838cc
Tristan Garzon <47580382+tristangarzon@users.noreply.github.com>
2021-04-11 21:24:56 -0400

    Teleportation
    Player:
    -The player is now able to teleport around certain parts of the world using the teleport ray which is attached to the right hand of the player
    - The ray shoots out an arch indicator showing the player the targeted destination.
    - There are two fields in which the player can teleport to.
    1. Free open Teleportation Area, the player  is allowed to move anywhere within the area.
    2, Locked Teleportation Anchor, the player is locked to the center position of the anchored area.
    - The ray will only appear if the user clicks the trigger button past a certain threshold
    
    Other:
    Upgraded the project to use the universal rendering pipeline

---
+++ MultiplayerGame/Assets/Scripts/XR/LocomotionController.cs
@@ -0,0 +1,54 @@
+﻿/*
+* Made by: Tristan Garzon
+* 
+* Script Summary:
+*
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LocomotionController : MonoBehaviour
+{
+    //Reference to the leftTeleportRay
+    //Disabled for now, will add a feature to allow the player to select which hand they want to usedwn
+    public XRController leftTeleportRay;
+
+    //Reference to the rightTeleportRay
+    public XRController rightTeleportRay;
+
+    public InputHelpers.Button teleportActivationButton;
+    public float activationThreshold = 0.1f;
+
+    #region Variables
+
+    #endregion
+	
+    #region Unity Methods
+
+   
+    void Update()
+    {
+        //Checks if Left ray is active
+        if(leftTeleportRay)
+        {
+            leftTeleportRay.gameObject.SetActive(CheckIfActivated(leftTeleportRay));
+        }
+
+        if (rightTeleportRay)
+        {
+            rightTeleportRay.gameObject.SetActive(CheckIfActivated(rightTeleportRay));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)  //Checks to see if the ray is currently active
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, activationThreshold);
+        return isActivated;
+           
+    }
+
+    #endregion
+}
