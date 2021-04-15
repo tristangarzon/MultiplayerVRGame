1e30bbef6b850d35950894c8a5cc0acff4b361f9
Tristan Garzon <47580382+tristangarzon@users.noreply.github.com>
2021-04-15 18:59:05 -0400

    Snap Zones / Drop Zones
    Snap Zones:
    - Added snap zones placed around the world, players can place items inside the snap zones and the items will snap to the zone and stay within the confines of the zone.
    - Added Snap zones for the player, now the player is able to place objects on its head as well as being able to carry objects on its hip

---
+++ MultiplayerGame/Assets/Scripts/Characters/FollowToTheSide.cs
@@ -0,0 +1,33 @@
+﻿/*
+* Made by: Tristan Garzon
+* 
+* Script Summary:
+*
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowToTheSide : MonoBehaviour
+{
+    #region Variables
+    public Transform target; // Reference to the VR Camera
+    public Vector3 offset;
+    #endregion
+
+    #region Unity Methods
+
+    void FixedUpdate()
+    {
+        //Makes the object follow the player
+        transform.position = target.position + Vector3.up * offset.y
+            + Vector3.ProjectOnPlane(target.right, Vector3.up).normalized * offset.x
+            + Vector3.ProjectOnPlane(target.forward, Vector3.up).normalized * offset.z;
+
+        //Makes the object rotate with the player
+        transform.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+    }
+
+    #endregion
+}
