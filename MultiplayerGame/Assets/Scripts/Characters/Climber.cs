13b5e1e01b5a6e6680f1516bc521c7dbbcf950d8
Tristan Garzon <47580382+tristangarzon@users.noreply.github.com>
2021-04-17 20:47:12 -0400

    Climbing
    Player Movement:
    - Add a rock climbing wall into the game that the player can interact with, the player is able to grab onto the rocks found on the wall and grab onto them
    - The player has the ability to climb the wall

---
+++ MultiplayerGame/Assets/Scripts/Characters/Climber.cs
@@ -0,0 +1,54 @@
+﻿/*
+* Made by: Tristan Garzon
+* 
+* Script Summary:
+*
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Climber : MonoBehaviour
+{
+    #region Variables
+    private CharacterController character;  //Reference to character
+    public static XRController climbingHand;       //Reference to the hands
+    private DeviceBasedContinuousMoveProvider contMovement;
+    #endregion
+	
+    #region Unity Methods
+
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        contMovement = GetComponent<DeviceBasedContinuousMoveProvider>();
+    }
+
+    void FixedUpdate()
+    {
+        //Will disable the movement of the character if the character is climbing
+        if(climbingHand)
+        {
+            contMovement.enabled = false;
+            Climb();
+        }
+        else
+        {
+            contMovement.enabled = true;
+        }
+    }
+
+    void Climb() //Allows the player to climb
+    {
+        //Finds the velocity of the climbing hand
+        InputDevices.GetDeviceAtXRNode(climbingHand.controllerNode).TryGetFeatureValue(CommonUsages.deviceVelocity, out Vector3 velocity);
+
+        //Moves our body in the opposite direction
+        character.Move(transform.rotation * -velocity * Time.fixedDeltaTime);
+    }
+
+    #endregion
+}
---
+++ MultiplayerGame/Assets/Scripts/Objects/ClimbInteractable.cs
@@ -0,0 +1,45 @@
+﻿/*
+* Made by: Tristan Garzon
+* 
+* Script Summary:
+*
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ClimbInteractable : XRBaseInteractable
+{
+    #region Variables
+
+    #endregion
+
+    #region Unity Methods
+
+    protected override void OnSelectEntered(XRBaseInteractor interactor)
+    {
+        base.OnSelectEntered(interactor);
+
+        //Makes sure it isnt the xr ray that is interacting with the stones
+        if(interactor is XRDirectInteractor)
+        Climber.climbingHand = interactor.GetComponent<XRController>(); //Gets a reference of to the controller 
+    }
+
+    protected override void OnSelectExited(XRBaseInteractor interactor)
+    {
+        base.OnSelectExited(interactor);
+
+
+        if(interactor is XRDirectInteractor)
+        {
+            if(Climber.climbingHand && Climber.climbingHand.name == interactor.name)
+            {
+                Climber.climbingHand = null;
+            }
+        }
+    }
+
+    #endregion
+}
