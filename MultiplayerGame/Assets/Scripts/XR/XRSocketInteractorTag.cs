acb0952c53018dae89e8fd5c83a49e31532a8509
Tristan Garzon <47580382+tristangarzon@users.noreply.github.com>
2021-04-22 20:29:55 -0400

    Better Gun Weapon & Bugs
    Weapons:
    - Updated the pistol weapon in the scene to feel a bit more realistic
    - Gun has a proper firing animation, muzzle flash effect, and proper sound effects
    - Added Reload Snap zone to the weapon, now when the player puts the ammo clip near the gun it will snap to the weapon and reload the gun
    
    Bugs:
    - Fixed an issue where the Ray Interactor wasn't disabled when picking up an object, now when an object is held by the player, another object can't snap to the players hand

---
+++ MultiplayerGame/Assets/Scripts/XR/XRSocketInteractorTag.cs
@@ -0,0 +1,26 @@
+﻿/*
+* Made by: Tristan Garzon
+* 
+* Script Summary:
+*
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRSocketInteractorTag : XRSocketInteractor
+{
+    #region Variables
+    public string targetTag;
+    #endregion
+
+    #region Unity Methods
+
+    public override bool CanSelect(XRBaseInteractable interactable)
+    {
+        return base.CanSelect(interactable) && interactable.CompareTag(targetTag);
+    }
+    #endregion
+}
