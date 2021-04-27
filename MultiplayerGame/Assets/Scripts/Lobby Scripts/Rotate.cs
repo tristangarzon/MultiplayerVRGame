bec28a7e3fd1452ef8adb8f1b101989cde6fdbee
Tristan Garzon <47580382+tristangarzon@users.noreply.github.com>
2021-04-26 20:20:53 -0400

    Updated Lobby
    - Added a character preview to the Lobby when the player is selecting a character
    - Changed the Skybox
    
    **Side Note**
    The first part of the project is finished, next I will be working on the VR Zombie Game.

---
+++ MultiplayerGame/Assets/Scripts/Lobby Scripts/Rotate.cs
@@ -0,0 +1,27 @@
+﻿/*
+* Made by: Tristan Garzon
+* 
+* Script Summary:
+*
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]
+    private float rotationSpeed = 15f;
+    #endregion
+	
+    #region Unity Methods
+   
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+    }
+
+    #endregion
+}
